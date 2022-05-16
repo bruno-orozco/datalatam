58da81b521dd602d7906a8ebdc58a366efc33039
Bruno Orozco <orozco.bruno@comunidad.unam.mx>
2022-05-15 20:31:48 -0500

    first commit

---
+++ datalatam/src/main/java/com/datlatamproject/springboot/datalatam/DatalatamApplication.java
@@ -0,0 +1,13 @@
+package com.datlatamproject.springboot.datalatam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatalatamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatalatamApplication.class, args);
+	}
+
+}
---
+++ datalatam/src/test/java/com/datlatamproject/springboot/datalatam/DatalatamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.datlatamproject.springboot.datalatam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatalatamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
